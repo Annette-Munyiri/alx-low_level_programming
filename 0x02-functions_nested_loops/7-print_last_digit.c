bf88f6399213eb2550a1382ccdb13fb31838bb21
Annette-Munyiri <anmunyiri390.gmail.com>
2021-06-22 10:33:32 -0700

    COrrected Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,15 +7,10 @@
  */
 int print_last_digit(int n)
 {
-	int x;
+	int x = n % 10;
 
-	if (n < 0)
-		n = -n;
-
-	x = n % 10;
-
-	if (x < 10)
-		x = -x;
+	if (x < 0)
+		x *= -1;
 
 	_putchar(x + '0');
 
