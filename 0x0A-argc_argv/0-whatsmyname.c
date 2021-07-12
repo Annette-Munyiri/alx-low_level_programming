0d1a90d5b9367e312cac61778d49c8cbcf1022c4
Annette-Munyiri <anmunyiri390.gmail.com>
2021-07-11 23:59:06 -0700

    Finished Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints program name
+ * Return: Always 0 (On success)
+ * @argc: no of arguments
+ * @argv: argument
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
