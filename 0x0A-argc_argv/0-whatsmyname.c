a5c72dffbeca55a66a6e029670e72481149be946
Annette-Munyiri <anmunyiri390.gmail.com>
2021-07-12 03:47:37 -0700

    Completed task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * @argc: no of arguments
  * @argv: argument
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
