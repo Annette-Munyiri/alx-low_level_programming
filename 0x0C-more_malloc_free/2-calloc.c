5bdf3fc7c18599eff5c43e9f329491fa40b298f4
Annette-Munyiri <anmunyiri390.gmail.com>
2021-07-15 10:16:29 -0700

    Completed task

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,25 @@
-#include "holberton.h"
 #include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
+
 
 /**
  * _calloc - allocates memory for an array, using malloc
@@ -10,22 +30,17 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *x;
-	char *f;
-	unsigned int i;
+	char *ptr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	x = malloc(size * nmemb);
+	ptr = malloc(size * nmemb);
 
-	if (x == NULL)
+	if (ptr == NULL)
 		return (NULL);
 
-	f = x;
+	_memset(ptr, 0, nmemb * size);
 
-	for (i = 0; i < (size * nmemb); i++)
-		f[i] = '\0';
-
-	return (x);
+	return (ptr);
 }
--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,5 +1,5 @@
-#include "holberton.h"
 #include <stdlib.h>
+#include "holberton.h"
 
 /**
  * array_range - creates an array of integers
@@ -9,20 +9,21 @@
  */
 int *array_range(int min, int max)
 {
-	int *a, i, s;
+	int *ptr;
+	int i, size;
 
 	if (min > max)
 		return (NULL);
 
-	s = max - min + 1;
+	size = max - min + 1;
 
-	a = malloc(sizeof(int) * s);
+	ptr = malloc(sizeof(int) * size);
 
-	if (array == NULL)
+	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; i < s; i++)
-		a[i] = min++;
+	for (i = 0; min <= max; i++)
+		ptr[i] = min++;
 
-	return (a);
+	return (ptr);
 }
