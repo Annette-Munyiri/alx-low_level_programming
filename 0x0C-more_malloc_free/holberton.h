fcb3b843540bc140044cad579d3e842bb181fcff
Annette-Munyiri <anmunyiri390.gmail.com>
2021-07-15 23:43:33 -0700

    Completed task

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -5,7 +5,7 @@
 /*
  * File: holberton.h
  */
-int _putchar(char *c);
+int _putchar(int c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
